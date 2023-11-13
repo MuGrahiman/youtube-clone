7de11cad5d64be2f4d8e1708c45c4f8d28f2a99f
MuGrahiman <mujeebrahiman2000@gmail.com>
2023-11-13 15:13:05 +0530

    changed server as submodule

--- server/Models/watchLater.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-const watchLaterSchema = mongoose.Schema({
-  videoId: { type: String, required: true },
-  Viewer: { type: String, required: true },
-  LikedOn: { type: Date, default: Date.now() },
-});
-export default mongoose.model("WatchLater", watchLaterSchema);
